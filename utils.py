dc86f5f9f73fafa025fe24d7154ccba2c32fb524
briansu <brianbb.su@gmail.com>
2019-09-26 20:37:05 +0800

    Fix: escape content before sending telegram notification

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 from sqlalchemy import desc
 import requests
+import re
 from models import Session, Announcement
 
 import config
+
+def escape_markdown_text(s):
+    return re.sub(r"([_*`])", r"\\\1", s)
 
 def get_submission_list():
     session = Session()
@@ -11,7 +15,8 @@ def get_submission_list():
 
 def push_telegram_notification(anno, chat_id):
     url = "https://api.telegram.org/bot{}/sendMessage".format(config.telegram_bot_token)
-    text = f"*{anno.classname} - {anno.title}*\n\n{anno.get_text_content()}\n\n"
+    content = escape_markdown_text(anno.get_text_content())
+    text = f"*{anno.classname} - {anno.title}*\n\n{content}\n\n"
     text += f"[Announcement Link]({anno.url})\n_Posted at: {anno.date_str()}_"
     r = requests.post(url, json = {
         "chat_id": chat_id,
