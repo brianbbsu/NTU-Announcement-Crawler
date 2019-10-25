da817dd9647396e2cc27ae5fc52ffe96227a5090
prpr <3254tg89@gmail.com>
2019-10-25 15:09:49 +0800

    Add crawler for Computer Networks 2019 (AC Pang) (#3)
    * Add new crawler for Computer Networks 2019 (AC Pang)

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -3,3 +3,4 @@ from .base import BaseCrawler
 from .NTU_ceiba import CeibaCrawler
 from .NTU_cool import NTUCoolCrawler
 from .CN2019 import CN2019Crawler
+from .CN2019_AC_Pang import CN2019_AC_Pang_Crawler
